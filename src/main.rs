a1f15343ae2a58ad3527d984595966cb2359c827
toyboot4e <toyboot4e@gmail.com>
2023-02-11 16:25:59 +0900

    simple http server with `warp` and `tokio`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    // access `localhost:3030/status`
+    let status = warp::path!("status").map(|| warp::reply::html("hello"));
+
+    let port = 3030;
+    warp::serve(status).run(([0, 0, 0, 0], port)).await;
 }
